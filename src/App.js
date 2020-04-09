5baca858406d8f9955ee79a6c4b959e75055fa2e
pinglu85 <luping8576@hotmail.com>
2020-04-09 05:03:34 +0200

    Change the folder structure

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import React, { useEffect, useRef } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import styles from './App.module.css';
-import Button from './components/UI/Button/Button';
+import Form from './container/Form/Form';
+import Game from './container/Game/Game';
 
 function App() {
-  const ref = useRef(null);
-
-  useEffect(() => {
-    ref.current.focus();
-  }, []);
-
   return (
     <Router>
-      <div className={styles.App}>
-        <header>Tic Tac Toe</header>
-        <form>
-          <label>
-            Player Name
-            <input
-              ref={ref}
-              type="text"
-              name="username"
-              placeholder="Enter player name"
-              value=""
-            />
-          </label>
-          <Button>Join</Button>
-        </form>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Form}></Route>
+          <Route path="/game" component={Game}></Route>
+        </Switch>
       </div>
     </Router>
   );
