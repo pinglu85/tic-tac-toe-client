54fdb0ab2920b8102312bed6a4466ddeae8f07f8
pinglu85 <luping8576@hotmail.com>
2020-04-11 05:24:50 +0200

    first commit

---
+++ src/components/UI/GameHeader/GameHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Player from './Player/Player';
+
+const GameHeader = () => {
+  return (
+    <header>
+      <Player />
+    </header>
+  );
+};
+
+export default GameHeader;
